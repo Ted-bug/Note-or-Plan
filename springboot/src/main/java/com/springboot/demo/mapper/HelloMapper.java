5890ef5a7c5a3f5f0b99690825bf68c82b489d9f
Ted-bug <64293474+Ted-bug@users.noreply.github.com>
2022-03-11 20:47:13 +0800

    Delete springboot directory

--- springboot/src/main/java/com/springboot/demo/mapper/HelloMapper.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.demo.mapper;
-
-import com.springboot.demo.pojo.Test;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HelloMapper {
-    Integer getCount();
-}
--- springboot/src/main/java/com/springboot/demo/service/HelloService.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.demo.service;
-
-import com.springboot.demo.pojo.Test;
-
-public interface HelloService {
-    Integer getCount();
-}
